e3dfdd486e15bc77b94008f03af57bffc2199e91
nidia <nidiacmh@gmail.com>
2021-01-05 17:38:07 -0600

    interfaz de auth0

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PreciosComponent } from './components/precios/precios.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProtegidaComponent } from './components/protegida/protegida.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home', component: HomeComponent},
+  {path:'precios', component: PreciosComponent},
+  {path:'protegida', component: ProtegidaComponent},
+  {path:'**', pathMatch:'full', redirectTo:'home'}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
